2e22f22318390f0c3774c9b12dbec9d40bd0e562
Gregory Row <rowgregory@gmail.com>
2026-07-16 22:45:05 -0400

    RG embedded toolkit needs to be allowed in frame-src next config

--- next.config.ts
+++ next.config.ts
@@ -44,7 +44,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://www.gstatic.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://firebasestorage.googleapis.com https://cdn.rescuegroups.org https://lh3.googleusercontent.com https://platform-lookaside.fbsbx.com https://www.gstatic.com https://fonts.gstatic.com",
-              "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://translate.google.com",
+              "frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://translate.google.com https://toolkit.rescuegroups.org",
               "connect-src 'self' https://api.stripe.com https://firebasestorage.googleapis.com wss://ws-us2.pusher.com https://sockjs-us2.pusher.com wss://ws-us3.pusher.com https://sockjs-us3.pusher.com https://www.google-analytics.com https://translate.googleapis.com https://translate-pa.googleapis.com",
               "media-src 'self' https://firebasestorage.googleapis.com"
             ].join('; ')
